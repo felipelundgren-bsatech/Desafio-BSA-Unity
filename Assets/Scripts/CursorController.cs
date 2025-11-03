72bda3ba82852c1ead54c64403971f5adf89258c
felipelundgren-bsatech <felipe.lundgren@bsatech.io>
2025-11-03 17:52:39 -0300

    feat: criado cenario de batalha e começando a bag

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+public class CursorController: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    
+    public GameObject cursorImage; 
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (cursorImage != null)
+        {
+            
+            cursorImage.SetActive(true);
+        }
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (cursorImage != null)
+        {
+            
+            cursorImage.SetActive(false);
+        }
+    }
+}
